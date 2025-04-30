01df396c4d010561f1ea6fa2f9262359d55845d2
sukhdev pratap singh <sukhdevps04@gmail.com>
2025-04-30 22:38:30 +0530

    adding the controllers , models , and the rest things

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+login_manager = LoginManager()
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.settings.Config')
+    
+    db.init_app(app)
+
+    login_manager.init_app(app)
+
+    from config.commands import register_commands
+    register_commands(app)
+
+    return app
+
